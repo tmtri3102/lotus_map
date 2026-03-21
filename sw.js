e3d169f38a2c900ec489189f77f9bf66e3e8a616
Tran Minh Tri <tmtri310251@gmail.com>
2026-03-22 06:18:15 +0700

    add favicon

--- sw.js
+++ sw.js
@@ -1,36 +1,48 @@
-const CACHE_NAME = 'lotus-map-v2';
+const CACHE_NAME = "lotus-map-v2";
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/demo.geojson',
-  '/style.css',
-  '/main.js',
-  'https://api.mapbox.com/mapbox-gl-js/v3.20.0/mapbox-gl.css',
-  'https://api.mapbox.com/mapbox-gl-js/v3.20.0/mapbox-gl.js'
+  "/",
+  "/index.html",
+  "/demo.geojson",
+  "/style.css",
+  "/main.js",
+  "/public/favicon.ico",
+  "/public/favicon-16x16.png",
+  "/public/favicon-32x32.png",
+  "/public/apple-touch-icon.png",
+  "/public/logo_icon.png",
+  "/public/logo_small.png",
+  "/public/logo_large.png",
+  "https://api.mapbox.com/mapbox-gl-js/v3.20.0/mapbox-gl.css",
+  "https://api.mapbox.com/mapbox-gl-js/v3.20.0/mapbox-gl.js",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (!event.request.url.startsWith('http')) return;
+self.addEventListener("fetch", (event) => {
+  if (!event.request.url.startsWith("http")) return;
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) return cachedResponse;
       return fetch(event.request).then((response) => {
-        // Only cache Mapbox Tiles / Assets or our own api to save requests. 
+        // Only cache Mapbox Tiles / Assets or our own api to save requests.
         // We will omit eleven labs from SW cache initially to prevent heavy bloat or TTS cache issues since using S3
-        if (event.request.url.includes("mapbox.com") || event.request.url.includes("lotus-map")) {
-           const cloned = response.clone();
-           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+        if (
+          event.request.url.includes("mapbox.com") ||
+          event.request.url.includes("lotus-map")
+        ) {
+          const cloned = response.clone();
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, cloned));
         }
         return response;
       });
-    })
+    }),
   );
 });
